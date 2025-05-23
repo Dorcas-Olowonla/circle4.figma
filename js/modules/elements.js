188b269108a9bb675b006cf8c6712556dd5915bc
andrejonn31 <ca.ozommajonn@gmail.com>
2025-05-23 12:06:45 +0100

    updated files
    Made changes to the styles.css to reflect exactly how the cards should be arranged. Added the url for all images in order for us to have the exact images from Figma. Updated the other files.

--- js/modules/elements.js
+++ js/modules/elements.js
@@ -28,6 +28,5 @@ export const elements = {
   uploadText: document.querySelector("#uploadText"),
 
   // Card elements
-  cardSection: document.qyerySelector(".post-flex"),
   cardContainer: document.querySelector(".post-flex-row"),
 };
